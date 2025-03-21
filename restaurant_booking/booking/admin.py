4d2b227fa75cf6bf3d93e2322b3f4c6bab5aae9a
kumarborkar22 <borkarkumar22@gmail.com>
2025-03-21 19:22:12 +0530

    updates

--- restaurant_booking/booking/admin.py
+++ restaurant_booking/booking/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Customer, Table, Reservation
+from .models import Customer, Table, Reservation, Review
 
 admin.site.register(Customer)
 admin.site.register(Table)
 admin.site.register(Reservation)
+admin.site.register(Review)
